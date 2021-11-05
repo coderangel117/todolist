0fe2e7064b3dbe8086e934cf69f2064a0fcb1c19
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-05 23:58:33 +0100

    script to create task + mark task as done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let reload = document.getElementById("reload");
 let enter = document.getElementById("btn-submit-task");
 let InputUser = document.getElementById("create-task");
-let liste = document.getElementsByTagName("ul");
+let list = document.getElementsByTagName("ul");
 let task = document.getElementByTagName("li");
 
 function ReloadPage(){
@@ -10,11 +10,21 @@ function ReloadPage(){
 
 
 function inputLength(){
-	return input.value.length;
+	return input.value.length;// to test if input is not empty 
 } 
 
 function listLength(){
-	return task.length;
+	return task.length; // the number of tasks in the list 
 }
+let li = document.createElement("li");
+
+li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+
+function crossOut(){
+    li.classList.toggle("done");
+}
+li.addEventListener("click", crossOut);//mark task as done when it was clicked
 
 reload.addEventListener("click", ReloadPage);
