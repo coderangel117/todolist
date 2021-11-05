e13d6f5d26cf0e7b14c44306657232acc019f90f
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-05 23:30:35 +0100

    create variable and length function

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+let reload = document.getElementById("reload");
+let enter = document.getElementById("btn-submit-task");
+let InputUser = document.getElementById("create-task");
+let liste = document.getElementsByTagName("ul");
+let task = document.getElementByTagName("li");
+
 function ReloadPage(){
     location.reload();
 }
-let reload = document.getElementById("reload");
+
+
+function inputLength(){
+	return input.value.length;
+} 
+
+function listLength(){
+	return task.length;
+}
+
 reload.addEventListener("click", ReloadPage);
