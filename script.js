996aa93922aa5aed062c4054c0f4b913a6cbf4fd
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-06 13:40:57 +0100

    add button to delete task remove button reload page

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let reload = document.getElementById("reload");
 let enter = document.getElementById("btn-submit-task");
 let InputUser = document.getElementById("create-task");
 let list = document.getElementsByTagName("ul");
 let task = document.getElementByTagName("li");
 
-function ReloadPage(){
-    location.reload();
-}
 
 
 function inputLength(){
-	return input.value.length;// to test if input is not empty 
-} 
+	return input.value.length;// To return de length of input by user
+function listLength(){
+	return task.length; // return the number of tasks in the list 
+}
 
-
+// START ADD TASK TO LIST
 function addListAfterClick(){
 	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
 		createListElement();
@@ -26,18 +24,40 @@ function addListAfterKeypress(e) {
 		createListElement();
 	} 
 }
-function listLength(){
-	return task.length; // return the number of tasks in the list 
-}
+// END ADD TASK TO LIST
+enterButton.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+//START ADD ITEM LIST 
+
 let li = document.createElement("li");
 
 li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
+	list.appendChild(li); //adds li to ul
 	input.value = ""; //Reset text input field
 
+//END ADD ITEM LIST 
+
+
+//START MARK AS DONE OPTION
 function crossOut(){
     li.classList.toggle("done");// change the statement of the class (true <--> false) 
 }
 
 li.addEventListener("click", crossOut);//mark task as done when it was clicked
-reload.addEventListener("click", ReloadPage);
+
+// END MARK AS DONE OPTION
+
+// START ADD DELETE BUTTON
+var dBtn = document.createElement("button");
+dBtn.appendChild(document.createTextNode("X"));
+li.appendChild(dBtn);
+dBtn.addEventListener("click", deleteListItem);
+// END ADD DELETE BUTTON
+
+//ADD CLASS DELETE (DISPLAY: NONE)
+function deleteListItem(){
+	li.classList.add("delete")
+}
+//END ADD CLASS DELETE
