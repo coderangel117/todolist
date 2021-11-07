09663dbafe9fd7419e0c7e5543576cad8396883b
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-07 01:39:11 +0100

    ✨ ca fonctionne! ajout et suppr plus que le style

--- script.js
+++ script.js
@@ -1,65 +1,64 @@
-let enter = document.getElementById("btn-submit-task");
-let InputUser = document.getElementById("create-task");
-let list = document.getElementsByTagName("ul");
-let task = document.getElementByTagName("li");
-
-
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
 
 function inputLength(){
-	return input.value.length;// To return de length of input by user
-	function listLength(){
-		return task.length; // return the number of tasks in the list 
-	}
-}
-//START ADD ITEM LIST 
-function createListElement() {
-	let li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	list.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
-}
-//END ADD ITEM LIST 
+	return input.value.length;
+} 
 
-// START ADD TASK TO LIST
+function listLength(){
+	return item.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	// li.innerHTML('<p>'+ input.value  + '</p>');
+	input.value = ""; //Reset text input field
+
+
+	//START STRIKETHROUGH
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");
+	}
+
+	li.addEventListener("click",crossOut);
+	//END STRIKETHROUGH
+
+
+	// START ADD DELETE BUTTON
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+	// END ADD DELETE BUTTON
+
+
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
+}
+
+
 function addListAfterClick(){
 	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
 		createListElement();
 	}
 }
 
-function addListAfterKeypress(e) {
-	if (inputLength() > 0 && e.which ===13) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by e.keyCode === 13
-		createListElement();
-	} 
-}
-// END ADD TASK TO LIST
+ function addListAfterKeypress(event) {
+ 	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
+ 		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+ 		createListElement();
+ 	} 
+ }
+
+
 enterButton.addEventListener("click",addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
-
-
-//START MARK AS DONE OPTION
-function crossOut(){
-    li.classList.toggle("done");// change the statement of the class (true <--> false) 
-}
-
-li.addEventListener("click", crossOut);//mark task as done when it was clicked
-
-// END MARK AS DONE OPTION
-
-//ADD CLASS DELETE (DISPLAY: NONE)
-function deleteListItem(){
-	li.classList.add("delete")
-}
-//END ADD CLASS DELETE
-
-// START ADD DELETE BUTTON
-let dBtn = document.createElement("button");
-dBtn.appendChild(document.createTextNode("X"));
-li.appendChild(dBtn);
-dBtn.addEventListener("click", deleteListItem);
-// END ADD DELETE BUTTON
-
-
