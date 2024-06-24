f05dc0bb4516a8f59996d25ffb80395d516f574e
gab <perinogabriel@gmail.com>
2024-06-24 19:35:11 +0200

    Revert "refactor [script] : reformat code"
    This reverts commit c94c5eca537d2293d6ec053896c570be1be20596.

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ function addListAfterKeypress(event) {
 }
 
 
-window.addEventListener("load", () => {
+window.addEventListener("load", (event) => {
     let Tasks = localStorage.getItem("TasksList") ?? '[]'
     Tasks = JSON.parse(Tasks)
     if (Tasks.length > 0) {
